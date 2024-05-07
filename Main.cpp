7b18564d20c5a752d34f3184295f0c58cccb6997
marlinverhulst <marlinverhulst@gmail.com>
2024-05-07 20:28:58 +0200

    Corrected typo's in shaders

--- Main.cpp
+++ Main.cpp
@@ -4,17 +4,17 @@
 
 
 const char* vertexShaderSource = "#version 330 core\n"
-"Layout (location = 0) in vec3 aPos;\n"
+"layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
-"	gl_Position = vec4(a.pos.x, aPos.y, aPos.z, 1.0);\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(0.4f, 0.3f, 0.02f, 1.0);\n"
+"	FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
 "}\0";
 
 
